498491d407055a1e96cbc56995461000c56b01cf
atkhamov <atkhamov@gmail.com>
2020-10-15 21:12:50 +0600

    2020-10-15 important

---
+++ newsreports/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NewsreportsConfig(AppConfig):
+    name = 'newsreports'
--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'pages/index.html')
 
-def about(request):
-    return render(request, 'pages/about.html')
+def newsreport(request):
+    return render(request, 'pages/newsreport.html')
 
